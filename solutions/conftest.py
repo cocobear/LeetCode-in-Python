f64b7c79e179f6706008ddf45b7e1c083b5f7a0e
cocobear <cocobear.cn@gmail.com>
2020-01-17 16:34:27 +0800

    修正有多个方法时未能全部调用的错误

--- solutions/conftest.py
+++ solutions/conftest.py
@@ -17,18 +17,9 @@ def pytest_pycollect_makeitem(collector, name, obj):
         # Split into (args, expected)
         cases = [(c[:-1], c[-1]) for c in cases]
 
-        # @pytest.mark.parametrize('func', funcs)
-        # @pytest.mark.parametrize(['args', 'expected'], cases)
-        # def test(func, args, expected):
-        #     assert getattr(obj(), func)(*args) == expected
+        @pytest.mark.parametrize('func', funcs)
+        @pytest.mark.parametrize(['args', 'expected'], cases)
+        def test(func, args, expected):
+            assert getattr(obj(), func)(*args) == expected
 
-        # return list(collector._genfunctions('test_' + obj.__module__, test))
-        test_list = []
-        for func in funcs:
-            @pytest.mark.parametrize(['args', 'expected'], cases)
-            def test(args, expected):
-                assert getattr(obj(), func)(*args) == expected
-
-            test_list.extend(list(collector._genfunctions('test_' + func, test)))
-
-        return test_list
+        return list(collector._genfunctions('test_' + obj.__module__, test))
