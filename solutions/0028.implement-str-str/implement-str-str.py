ad83ea1f98929e305e725a9a15a26aec486273cf
cocobear <cocobear.cn@gmail.com>
2020-05-07 15:47:19 +0800

    28 add new solution

--- solutions/0028.implement-str-str/implement-str-str.py
+++ solutions/0028.implement-str-str/implement-str-str.py
@@ -6,7 +6,7 @@
 
 # @lc code=start
 class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
+    def strStr_1(self, haystack: str, needle: str) -> int:
         n = len(haystack)
         m = len(needle)
         i = 0
@@ -21,6 +21,55 @@ class Solution:
                     break
                 j += 1
             if j == m:
+                return i
+            else:
+                i += 1
+        return -1
+
+    def strStr_2(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+        if not haystack:
+            return -1
+
+        i = 0
+        j = 0
+        m = len(haystack)
+        n = len(needle)
+        find = False
+        last = 0
+        x = 0
+        while i < m and j < n:
+            x = x + 1
+            print('i:%d j:%d'%(i,j))
+            if haystack[i] != needle[j]:
+                if find:
+                    i = max(0, i - j + 1)
+                else:
+                    i += 1
+                j = 0
+                find = False
+            else:
+                find = True
+                i += 1
+                j += 1
+
+        if j == n and find:
+            return i - n
+        else:
+            return -1
+
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+        if not haystack:
+            return -1
+
+        m = len(haystack)
+        n = len(needle)
+        i = 0
+        while i < m:
+            if haystack[i:i+n] == needle:
                 return i
             else:
                 i += 1
